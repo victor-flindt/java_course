6bb0980214a71a699feeb30df2ad4d3911b227a4
victor-flindt <vicorflindt@gmail.com>
2021-10-01 14:01:25 +0200

    exercise 1 done passes all test

--- NumberCheck.java
+++ NumberCheck.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.math.*;
+
 public class NumberCheck {
 	public static void main(String[] args) {
-        try{System.out.println(NumberCheck.check("123"));
+        try{System.out.println(NumberCheck.check("32165546168632215835423155415435"));
 	}
 	catch (Exception e){
 		// catches exepctions if the input to the functions is not a number
@@ -13,10 +15,6 @@ public static boolean check(String input_string){
 	String check_sum ="";
 	String output_string ="";
 	boolean output_bool = false;
-	int final_checksum = 0;
-
-	// Creates a list of integers
-	ArrayList <Integer> int_list = new ArrayList <Integer>();
 	// handles exepction if input to function is "" 
 	if(input_string == ""){
 		System.out.println(output_string);
@@ -29,26 +27,32 @@ public static boolean check(String input_string){
 			if(((Math.abs((i-(input_string.length()-1)))))%2!=0)
 			{
 				if((number*2)>10){
-				number = ((number*2)%10)+1;
-				}else if(number<10){
-					number = number *2;
-				}
+					//System.out.print("inde 1: ");
+			 		number = ((number*2)%10)+1;
+					//System.out.println(number);
+			 	}else if(number<10){
+					//System.out.print("inde 2: ");
+			 		number = number *2;
+					//System.out.println(number);
+			 	}
 			}
 		check_sum = check_sum+number;
-		int_list.add(Integer.parseInt(check_sum));
 		}
-		for (int i = 0; i < int_list.size(); i++) {
-			final_checksum = final_checksum + int_list.get(i);
+		int sum = 0;
+		// sums all chars in the checksum string and returns the result as sum. 
+		for (char c : check_sum.replaceAll("\\D", "").toCharArray()) {
+			int digit = c - '0';
+			sum += digit;
 		}
-		if(final_checksum%10==0){
+		// does the final validation. 
+		if(sum%10==0){
 			output_string="accepted";
 			output_bool = true;
 		}else{
 			output_string="false";
 			output_bool = false;
 		}
-		return output_bool;
+	 	return output_bool;
+	 }
 	}
-	}
-
 }
