2a5530ba4b972ef1dac403cc500cf9ea1f131435
Victor-Flindt <victorflindt@gmail.com>
2021-10-22 00:18:37 +0200

    added 7.2 cat exercise

--- java_files/FirstClass.java
+++ java_files/FirstClass.java
@@ -5,31 +5,48 @@ package ugeseddel07;
 public class FirstClass {
 	private String name;
 	private int id; 
-
+	// using array list instead of array because "java"
+	// List<FirstClass> cat_register = new ArrayList <FirstClass>();
 	public static void main(String args[]){
 
 		FirstClass fc1 = new FirstClass("bla",32);
-		FirstClass fc2 = new FirstClass("blu",42);
-		FirstClass fc3 = new FirstClass("bli",52);
-		System.out.println(fc3.toString());
-		System.out.println(fc2.toString());
-		System.out.println(fc1.toString());
-	
+		fc1.firstclass_idmutator(99);
+		fc1.firstclass_namemutator("haha");
 
+		System.out.println(fc1.firstclass_id());
+       fe  fe 
 	}
-
-	//Contructor
+								// Contructor
 	public FirstClass(String name, int id){
 		this.name = name;
 		this.id = id;
 	}
+								// Methods //
 	
-	// Method
-	
+	// To string
 	public String toString() {
-
 		return (id + " - "+ name);
-
 	}
-	
+
+	// Obejct register of 10 cats 
+
+	// public add_cat(FirstClass object){
+	// 	cat_register.add(object);
+	// }
+
+	// Mutators // 
+	public void firstclass_namemutator(String new_name){
+		this.name=new_name; 
+	}
+	public void firstclass_idmutator(int new_id){
+		this.id=new_id;
+	}
+								// Accesors //
+	public String firstclass_name(){
+		return name; 
+	}
+	public int firstclass_id(){
+		return id;
+	}
+
 }
